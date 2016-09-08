1e4e45ab07c2a4d978f9432fc7bb527b8f3eb508
Marius <marius.krajauskas@gmail.com>
2016-09-08 15:16:56 +0300

    addded show completed ant search text reducers

---
+++ app/reducers/reducers.jsx
@@ -0,0 +1,22 @@
+export var searchTextReducer = (state = '', action)=>{
+  switch (action.type) {
+    case 'SET_SEARCH_TEXT':
+      return action.searchText;
+      break;
+    default:
+      return state;
+
+  }
+}
+
+
+export var showCompletedReducer = (state = false, action)=>{
+  switch (action.type) {
+    case 'TOGGLE_SHOW_COMPLETED':
+      return !action.showCompleted;
+      break;
+    default:
+      return state;
+
+  }
+}
